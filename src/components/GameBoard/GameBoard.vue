e908002257ff2f4ca11fc0abc4ed62c95221ebdf
Charles Grandison Finney <therealakeem03@gmail.com>
2023-11-15 14:20:13 +0100

    Seperated game page and the game board component and improved the game logic

---
+++ src/components/GameBoard/GameBoard.vue
@@ -0,0 +1,206 @@
+<template lang="">
+    <div class="life__lines">
+        <span>Life lines:</span>
+        <button :disabled="lifelines.fiftyFifty" @click="useFiftyFifty" class="life__btn fifty__fity">
+            50:50
+        </button>
+        <button :disabled="lifelines.phoneAFriend" @click="usePhoneAFriend" class="life__btn phone__a__friend">
+            Call
+        </button>
+    </div>
+    <div class="question__display">
+        <h2 v-html="currentQuestion"></h2>
+    </div>
+    <div class="options__container">
+        <ul>
+            <li v-for="(option, index) in options" :key="index">
+                <button :disabled='answered' @click='selectOption(option)'>
+                    <SvgSpinners12DotsScaleRotate v-if="isChecking && selectedOption === option"/>
+                    <span v-else>{{ option }}</span>
+                </button>
+            </li>
+        </ul>
+    </div>            
+</template>
+<script setup>
+import { ref, computed, watch, onMounted, defineEmits } from 'vue'
+import SvgSpinners12DotsScaleRotate from '../../assets/icons/SvgSpinners12DotsScaleRotate.vue'
+
+// props defination
+
+const questionsData = ref({
+responsecode: 0,
+results: [
+{
+    category: "Entertainment: Japanese Anime & Manga",
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'Who was the first man to ever kick ass?',
+    correct_answer: 'Jackie Chan',
+    incorrect_answers: ['Bruce Lee', 'Arnold Schwazeneger', 'Sylvester Stallone']
+}, {
+    category: "Entertainment: Japanese Anime & Manga",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What year was &quot;JoJo&#039;s Bizarre Adventure: Phantom Blood&quot; first released?",
+    correct_answer: "1987",
+    incorrect_answers: ["2013", "1983","1995"]
+}, {
+    "category": "Entertainment: Japanese Anime & Manga",
+    "type": "multiple",
+    difficulty: "hard",
+    question: "In what year did the manga &quot;Ping Pong&quot; begin serialization?",
+    correct_answer: "1996",
+    incorrect_answers: ["2014", "2010", "2003"]
+}
+, {
+    "category": "Entertainment: Japanese Anime & Manga",
+    "type": "multiple",
+    difficulty: "hard",
+    question: "Who was the protagonist in the series PEAKY BLINDERS?",
+    correct_answer: "Tommy Shelby",
+    incorrect_answers: ["Arthur Shelby", "Suzzan", "Flitwick"]
+}
+, {
+    "category": "Entertainment: Japanese Anime & Manga",
+    "type": "multiple",
+    difficulty: "hard",
+    question: "What series has a villian named Lord Voldemort?",
+    correct_answer: "Harry Potter",
+    incorrect_answers: ["Merlin", "Robinhood", "Dominion"]
+}
+]
+})
+
+// Variable Definitions
+const emits = defineEmits(['incrementAmount', 'endGame'])
+const isPlaying = ref(true)
+const currentQuestionIndex = ref(0)
+const options = ref([])
+let currentAmountIndex = defineProps(['currentAmountIndex'])
+const selectedOption = ref(null)
+const isChecking = ref(false)
+const lifelines = ref({
+    phoneAFriend: false,
+    fiftyFifty: false
+})
+const friends = ref(['Eduardo', 'Wyatt', 'Chloe', 'Charlotte'])
+const answered = ref(false)
+const gameEnded = ref(false)
+
+
+const shuffleOptions = () => {
+    const correct_answer = questionsData.value.results[currentQuestionIndex.value].correct_answer
+    const incorrect_answers = questionsData.value.results[currentQuestionIndex.value].incorrect_answers
+    const optionsList = [correct_answer, ...incorrect_answers]
+
+    for (let i = optionsList.length -1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+
+        const temp = optionsList[i]
+        optionsList[i] = optionsList[j]
+        optionsList[j] = temp
+    }
+
+    // console.log(optionsArray)
+    options.value = optionsList
+}
+
+// Call the shuffleOptions function when the component is mounted or when the question changes
+onMounted(() => {
+    shuffleOptions();
+});
+
+watch(() => questionsData.value.results[currentQuestionIndex.value], () => {
+    shuffleOptions();
+});
+
+// Get the current Question
+const currentQuestion = computed(() => questionsData.value.results[currentQuestionIndex.value].question)
+
+// Lifelines functions
+const usePhoneAFriend = () => {
+    // Get a random friend
+    const randomFriendIndex = Math.floor(Math.random() * friends.value.length)
+    // Get random option
+    const randomOptionIndex = Math.floor(Math.random() * options.value.length)
+
+    lifelines.value.phoneAFriend = false
+
+    return [friends.value[randomFriendIndex, options.value[randomOptionIndex]]]
+}
+
+const useFiftyFifty = () => {
+
+    if (!answered.value) {
+        // clone options array to avoid modifying original array
+        const clonedOptions = [...options.value]
+
+        // Find correct option
+        const correctOptionIndex = options.value.findIndex(option => option === questionsData.value.results[currentQuestionIndex.value].correct_answer)
+
+        // remove two random incorrect options
+        let removedCount = 0
+        let currentIndex = 0
+
+
+        while (clonedOptions.length > 2) {
+            
+            if (clonedOptions[currentIndex] !== options.value[correctOptionIndex]) {
+                clonedOptions.splice(currentIndex, 1)
+                currentIndex++
+            } else {
+                currentIndex++
+                continue
+            }
+            removedCount++
+        }
+
+        // update options with the modified array
+        options.value = clonedOptions
+
+        // Disable lifeline
+        lifelines.value.fiftyFifty = true
+    }
+}
+
+const selectOption =(option) => {
+    selectedOption.value = option
+    isChecking.value = true
+    answered.value = true
+
+    setTimeout(() => {
+        if (option === questionsData.value.results[currentQuestionIndex.value].correct_answer) {
+        console.log('Correct')
+        if (currentQuestionIndex.value + 1 === questionsData.value.results.length) {
+            // End game
+            gameEnded.value = true
+        } else {
+            console.log(currentQuestionIndex.value, currentAmountIndex)
+            currentQuestionIndex.value++
+        }
+            isChecking.value = false
+            answered.value = false
+        } else {
+            // End Game
+            gameEnded.value = true
+
+            // console.log('Weird response. You failed!', winningAmount.value)
+        }
+    }, 3000)
+
+}
+
+watch(currentQuestionIndex, () => {
+    emits('incrementAmount')
+})
+
+watch(gameEnded, () => {
+    emits('endGame')
+})
+
+console.log('currentQuestionIndex: ' + currentQuestionIndex.value)
+</script>
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/Placeholder/Placeholder.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div class="placeholder__container">
+        <div class="placeholder__container-inner">
+            <div class="gif__container">
+                <div class="gif__container-gif"></div>
+            </div>
+            <div class="waiting__text">
+                <span>Set up game and click Start to begin!</span> 
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
